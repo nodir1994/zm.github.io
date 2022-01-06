1092aaa12c9a67d8253f91014b6217f4210eb4a7
nodir1994 <61582079+nodir1994@users.noreply.github.com>
2022-01-06 18:16:24 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
 
-    if ($(window).width() < 768) {
-        $('body ').addClass('open__side')
+    if ($(window).width() < 1023) {
+        $('.sidebar').append($('header ul'), $('.hdr__main .lang'))
+
+        $(document).on('click', '.hamburger', function(e){
+            $('body').addClass('opened')
+        })
+
+
+        $(document).on('click', 'body', function(e){
+            if (!$(e.target).is('.hamburger *, .hamburger, .sidebar, .sidebar *')) {
+                $('body').removeClass('opened')
+            }
+        })
+
     }
 
-    $('marquee').mouseover(function() {
-        $(this).attr('scrollamount',0);
-    }).mouseout(function() {
-         $(this).attr('scrollamount',5);
-    });
+    // $('marquee').mouseover(function() {
+    //     $(this).attr('scrollamount',0);
+    // }).mouseout(function() {
+    //      $(this).attr('scrollamount',5);
+    // });
 
-    $(document).on('click', '.select__main .top', function(){
-        $(this).parent().toggleClass('opened')
-    })
+    // $(document).on('click', '.select__main .top', function(){
+    //     $(this).parent().toggleClass('opened')
+    // })
 
  
 });
-    $(document).on('click', 'body', function(e){
-        if (!$(e.target).is('.select__main .top *, .select__main .top')) {
-            $('.select__main').removeClass('opened')
-        }
-    })
+    
